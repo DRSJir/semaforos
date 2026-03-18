ea62c551f244be317468c54834917f47d6a30a0b
DRSJir <jairdelreal@gmail.com>
2026-03-18 13:44:23 -0600

    Cambiar vista del monitoreo por tablas

--- monitoreo.sh
+++ monitoreo.sh
@@ -10,22 +10,31 @@ read -p "Ingresé el ID del proceso a monitoriar: " idSegmento
 numProc=$( ipcs -m $idSegmento | tail -n 2 | head -n 1 | awk '{print $6}')
 idSegmento=$( ipcs -m $idSegmento | tail -n 2 | head -n 1 | awk '{print $2}')
 
-echo -e "Hay $numProc procesos que usan el segmento $idSegmento \n"
-
 # Obtener el porceso creador, último proceso y usuario
 idProcCreate=$(ipcs -m -p $idSegmento | tail -n 2 | head -n 1 | awk '{print $3}')
 idUltimoProceso=$(ipcs -m -p $idSegmento | tail -n 2 | head -n 1 | awk '{print $4}')
 usuarioProceso=$(ipcs -m -p $idSegmento | tail -n 2 | head -n 1 | awk '{print $2}')
 
-echo -e "Creador Ultimo Usuario\n$idProcCreate $idUltimoProceso $usuarioProceso" | column -t
+echo -e "Hay $numProc procesos que usan el segmento $idSegmento \n"
+echo -e "Creador $idProcCreate \nUltimo  $idUltimoProceso \nUsuario $usuarioProceso\n"
+
+pids=()
+nombres=()
 
 # Buscamos en todos los procesos (/proc/[1-9]*)
 for proc_dir in /proc/[1-9]*/; do
     pid=$(basename $proc_dir)
 
     if grep -q "SYSV" "$proc_dir/maps" 2>/dev/null; then
-        nombre=$(cat "$proc_dir/comm")
-        echo "[MEMORIA] PID: $pid ($nombre) está usando el segmento $SHMID"
+        nombres+=("$(cat "$proc_dir/comm")")
+        pids+=("$pid")
 
     fi
 done
+
+{
+  echo "PID Nombre"
+  for i in "${!pids[@]}"; do
+      echo "${pids[$i]} ${nombres[$i]}"
+  done
+} | column -t
